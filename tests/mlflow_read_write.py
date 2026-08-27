0970d8e81b451fd45df9070882f79ebf887c4fb8
Francisco Vazquez de Sola Fernandez <fvazquezdesola@km3net.de>
2026-08-27 17:42:15 +0200

    Fix typose in mlflow_read_write

--- tests/mlflow_read_write.py
+++ tests/mlflow_read_write.py
@@ -1,7 +1,8 @@
 """Upload a metric and an artifact via MLflow, then read/download them."""
 
-from modules.mlflow_monitoring import ensure_experiment
+from importlib.resources import files, as_file
 
+from modules.mlflow_monitoring import ensure_experiment
 
 MLFLOW_EXP_NAME = "era5_drift_monitor_tests"
 MLFLOW_ARTIFACT_URI = "s3://eu-noth-1-an-fa-vs-era5-monitor/mlflow-tests"
@@ -9,20 +10,15 @@ MLFLOW_ARTIFACT_URI = "s3://eu-noth-1-an-fa-vs-era5-monitor/mlflow-tests"
 
 def main(): #pylint: disable=missing-function-docstring
 
-    exp_id = ensure_experiment(MLFLOW_EXP_NAME,
-                               MLFLOW_ARTIFACT_URI)
+    exp_id = ensure_experiment(MLFLOW_EXP_NAME, MLFLOW_ARTIFACT_URI)
 
     with mlflow.start_run(experiment_id = exp_id, run_name = run_name):
         print("artifact_uri:", mlflow.get_artifact_uri()) # TEST
 
         mlflow.log_param("input1", 1)
-	    mlflow.log_metric("output1", 1)
-
-	    artifact_filename = os.path.join("artifacts", k + ".npy")
-	    np.save(artifact_filename, v)
-	    mlflow.log_artifact(artifact_filename)
-	    os.remove(artifact_filename)
-
+        mlflow.log_metric("output1", 1)
+        with as_file(files("tests").joinpath("array_example.npy")) as path:
+            mlflow.log_artifact(path)
 
 if __name__ == "__main__":
     main()
