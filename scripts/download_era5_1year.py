fbe459848d2d579611ab99cfde857ee70fa8ba53
Francisco Vazquez de Sola Fernandez <fvazquezdesola@km3net.de>
2026-08-27 11:41:39 +0200

    Decrease amount of ERA5 downloaded in test

--- scripts/download_era5_1year.py
+++ scripts/download_era5_1year.py
@@ -1,17 +1,12 @@
 """Download one year of ERA5 data (Western Europe, low resolution). """
 
-import cdsapi
-c = cdsapi.Client()
+from modules.download_era5 import fetch_recent_year
 
-from modules.download_era5 import era5_request_fullyear, DATASET, PERIODS
-
-area_label = "western_europe"
-grid_label = "1deg"
-timestamp_label = "6h"
-
-for period_label, years in PERIODS.items():
-    for y in years:
-        target = f"era5_{area_label}_{grid_label}_{timestamp_label}_{period_label}_{y}.nc"
-        print("requesting", target)
-        c.retrieve(DATASET, era5_request_fullyear(y, area_label, grid_label, timestamp_label), target)
+fetch_recent_year(
+        year = 2020,
+        out_dir = os.getcwd(),
+        area_label = "western_europe",
+        grid_label = "1deg",
+        timestamp_label = "6h"
+        )
 
---
+++ scripts/download_era5_all.py
@@ -0,0 +1,17 @@
+"""Download "all" ERA5 data (Western Europe, low resolution, starting 1950s). """
+
+import cdsapi
+c = cdsapi.Client()
+
+from modules.download_era5 import era5_request_fullyear, DATASET, PERIODS
+
+area_label = "western_europe"
+grid_label = "1deg"
+timestamp_label = "6h"
+
+for period_label, years in PERIODS.items():
+    for y in years:
+        target = f"era5_{area_label}_{grid_label}_{timestamp_label}_{period_label}_{y}.nc"
+        print("requesting", target)
+        c.retrieve(DATASET, era5_request_fullyear(y, area_label, grid_label, timestamp_label), target)
+
